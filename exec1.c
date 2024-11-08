b6d937096f8dcac2563b296acb7778a0cbd8e3d5
Garbulinski <ionut-adrian.jurcaneanu@my.fmi.unibuc.ro>
2024-11-08 16:54:33 +0200

    commit exec1.c

--- exec1.c
+++ exec1.c
@@ -1,12 +1,12 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-
-int main(void)
-{
-	printf("This print is made from first program with proc id %d", getpid());
-	execl("./exec2","arg1", "arg2", "argc3", NULL);
-
-	printf("This part of the program will not be executed as this print\n");
-	return 0;
-}
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+int main() 
+{ 
+        printf("\n I am main process pid = (%d)\n",getpid());
+        execl("./exec2","arg1","arg2","arg3",NULL); 
+ 
+        printf("This line will not be printed... "); 
+      
+    return 0; 
+} 
